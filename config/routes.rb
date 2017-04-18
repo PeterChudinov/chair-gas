9ebd06de54806c4070c620fb5dc1fc085a1e4169
Peter Chudinov <peter@odalc.org>
2017-04-18 09:39:21 -0700

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'gas#index'
 
-  get 'gas/:state', to: 'gas#show'
+  get '/:state', to: 'gas#show'
 end
